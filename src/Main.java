bf0984d9d701085da86478f7d5fb6b4c231c2aed
tomlittlekim <youngmini90@naver.com>
2023-01-01 22:10:23 +0900

    가까운 글자 솔루션 추가

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 import min.babbling.Solution;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+//        babblingTest();
+//        closeLetterTest();
+    }
+
+    public static void babblingTest() {
         Solution solution = new Solution();
         String[] testString1 = {"aya", "yee", "u", "maa", "wyeoo"};
         String[] testString2 = {"ayaye", "uuuma", "ye", "yemawoo", "ayaa"};
@@ -9,4 +16,11 @@ public class Main {
         System.out.println(solution.solution(testString1));
         System.out.println(solution.solution(testString2));
     }
+
+    public static void closeLetterTest() {
+        closeLetter.Solution solution = new closeLetter.Solution();
+
+        System.out.println(Arrays.toString(solution.solution("banana")));
+    }
+
 }
---
+++ src/closeLetter/Solution.java
@@ -0,0 +1,18 @@
+package closeLetter;
+
+public class Solution {
+    public int[] solution(String s) {
+        // banana : [-1, -1, -1, 2, 2, 2]
+        int[] answer = new int[s.length()];
+
+        String bowl = "";
+        for (int i = 0; i < s.length(); i++) {
+            char temp = s.charAt(i);
+            int value = bowl.lastIndexOf(temp);
+            answer[i] = value == -1 ? value : i - value;
+            bowl += temp;
+        }
+
+        return answer;
+    }
+}
